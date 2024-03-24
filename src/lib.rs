b38bb0a0be30dcf40e0269dd078a52a05ff922a6
Goolnn <Goolnn@hotmail.com>
2024-03-24 22:11:28 +0800

    feat(lib)!: re-export all modules

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+mod credit;
+mod text;
+mod page;
+mod note;
+mod file;
+mod date;
+
+pub mod error;
+
+pub use credit::Credit;
+pub use text::Text;
+pub use page::Page;
+pub use note::Note;
+pub use file::File;
+pub use date::Date;
