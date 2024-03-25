45bb2ebd4c17fb57ab6a01633063c73e672cd60a
Goolnn <Goolnn@hotmail.com>
2024-03-25 19:28:25 +0800

    feat(note)!: implement 'Encode' and 'Decode' for 'Note'

--- src/note/mod.rs
+++ src/note/mod.rs
@@ -1,30 +1,32 @@
 use crate::text::Text;
-use crate::page::Page;
 
-pub struct Note<'a> {
-  parent: Option<&'a Page>,
-  
+use crate::error::{
+  FileResult,
+  FileError,
+};
+
+use crate::file::codec::{
+  Encode,
+  Decode,
+  Codec,
+};
+
+pub struct Note {
   x: f64,
   y: f64,
 
-  choice: usize,
+  choice: u32,
 
   texts: Vec<Text>,
 }
 
-impl<'a> Note<'a> {
-  pub fn new(parent: &'a Page) -> Self {
-    Self {
-      parent: Some(parent),
-      
-      ..Self::default()
-    }
+impl Note {
+  pub fn new() -> Self {
+    Self::default()
   }
 
-  pub fn with_coordinate_and_choice(parent: &'a Page, x: f64, y: f64, choice: usize) -> Self {
+  pub fn with_coordinate_and_choice(x: f64, y: f64, choice: u32) -> Self {
     Self {
-      parent: Some(parent),
-      
       x,
       y,
 
@@ -34,10 +36,8 @@ impl<'a> Note<'a> {
     }
   }
 
-  pub fn with_coordinate(parent: &'a Page, x: f64, y: f64) -> Self {
+  pub fn with_coordinate(x: f64, y: f64) -> Self {
     Self {
-      parent: Some(parent),
-      
       x,
       y,
 
@@ -53,7 +53,7 @@ impl<'a> Note<'a> {
     self.y = y;
   }
 
-  pub fn set_choice(&mut self, choice: usize) {
+  pub fn set_choice(&mut self, choice: u32) {
     self.choice = choice;
   }
 
@@ -65,7 +65,7 @@ impl<'a> Note<'a> {
     self.y
   }
 
-  pub fn choice(&self) -> usize {
+  pub fn choice(&self) -> u32 {
     self.choice
   }
 
@@ -78,17 +78,70 @@ impl<'a> Note<'a> {
   }
 }
 
-impl<'a> Default for Note<'a> {
+impl Default for Note {
   fn default() -> Self {
     Self {
-      parent: None,
-      
       x: 0.0,
       y: 0.0,
 
       choice: 0,
 
       texts: Vec::new(),
+    }
+  }
+}
+
+impl Encode for Note {
+  fn encode(&self, codec: &mut Codec) -> FileResult<()> {
+    match codec.version() {
+      (0, 2) => {
+        codec.write_primitive(self.x)?;
+        codec.write_primitive(self.y)?;
+
+        codec.write_primitive(self.choice)?;
+
+        codec.write_primitive(self.texts.len() as u32)?;
+
+        for text in &self.texts {
+          text.encode(codec)?;
+        }
+
+        Ok(())
+      }
+
+      _ => Err(FileError::InvalidVersion),
+    }
+  }
+}
+
+impl Decode for Note {
+  fn decode(codec: &mut Codec) -> FileResult<Self> {
+    match codec.version() {
+      (0, 2) => {
+        let x = codec.read_primitive::<f64>()?;
+        let y = codec.read_primitive::<f64>()?;
+
+        let choice = codec.read_primitive::<u32>()?;
+
+        let len = codec.read_primitive::<u32>()?;
+
+        let mut texts = Vec::with_capacity(len as usize);
+
+        for _ in 0..len {
+          texts.push(Text::decode(codec)?);
+        }
+
+        Ok(Self {
+          x,
+          y,
+
+          choice,
+
+          texts,
+        })
+      }
+
+      _ => Err(FileError::InvalidVersion),
     }
   }
 }
