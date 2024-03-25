ee926d0d6902187234bf9e653fff41047198ab87
Goolnn <Goolnn@hotmail.com>
2024-03-25 22:15:50 +0800

    feat(file)!: change field name from 'modified_date' to 'saved_date' in 'File'

--- src/file/mod.rs
+++ src/file/mod.rs
@@ -1,12 +1,14 @@
 pub(crate) mod codec;
 
-use crate::credit::Credit;
-use crate::page::Page;
-use crate::date::Date;
-
 use crate::error::{
   FileResult,
   FileError,
+};
+
+use crate::file::codec::{
+  Encode,
+  Decode,
+  Codec,
 };
 
 use std::collections::{
@@ -14,19 +16,27 @@ use std::collections::{
   HashMap,
 };
 
+use crate::credit::Credit;
+use crate::text::Text;
+use crate::page::Page;
+use crate::note::Note;
+use crate::date::Date;
+
+use regex::Regex;
+
 use std::path::Path;
 
 // 头部数据
 const HEADER_DATA: [u8; 15] = [0xE8, 0x8B, 0x8D, 0xE7, 0x9C, 0xBC, 0xE6, 0xB1, 0x89, 0xE5, 0x8C, 0x96, 0xE7, 0xBB, 0x84];
 
 // 版本数据
-const VERSION_0_0: [u8; 2] = [0x00, 0x00];
-const VERSION_0_1: [u8; 2] = [0x00, 0x01];
-const VERSION_0_2: [u8; 2] = [0x00, 0x02];
+const VERSION_0_0: (u8, u8) = (0x00, 0x00);
+const VERSION_0_1: (u8, u8) = (0x00, 0x01);
+const VERSION_0_2: (u8, u8) = (0x00, 0x02);
 
-const VERSION_LATEST: [u8; 2] = VERSION_0_2;
+const VERSION_LATEST: (u8, u8) = VERSION_0_2;
 
-const VERSIONS: [[u8; 2]; 3] = [
+const VERSIONS: [(u8, u8); 3] = [
   VERSION_0_0,
   VERSION_0_1,
   VERSION_0_2,
@@ -47,7 +57,7 @@ pub struct File {
   tags: HashSet<String>,
 
   created_date: Date,
-  modified_date: Date,
+  saved_date: Date,
 
   credits: HashMap<Credit, HashSet<String>>,
 
@@ -89,8 +99,8 @@ impl File {
     self.created_date
   }
 
-  pub fn modified_date(&self) -> Date {
-    self.modified_date
+  pub fn saved_date(&self) -> Date {
+    self.saved_date
   }
 
   pub fn credits_mut(&mut self) -> &mut HashMap<Credit, HashSet<String>> {
