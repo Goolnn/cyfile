bc214b19c0db38e73d71dbe3721ffbaf1919e069
Goolnn <Goolnn@hotmail.com>
2024-03-30 15:18:11 +0800

    feat(ffi): add trait 'Wrapped' for the struct who need to extern to implement

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,2 +1,14 @@
 mod file;
 mod text;
+mod note;
+
+trait Wrapped<T> {
+  fn new(raw: T) -> *mut Self;
+
+  fn raw(&self) -> &T;
+  fn raw_mut(&mut self) -> &mut T;
+
+  unsafe fn deref(ptr: *mut Self) -> &'static mut Self {
+    &mut *ptr
+  }
+}
