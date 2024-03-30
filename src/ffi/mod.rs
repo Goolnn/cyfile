aa814dd47cb2fbb5f855f978ecdb85c7b5d29b73
Goolnn <Goolnn@hotmail.com>
2024-03-30 16:03:57 +0800

    feat(ffi): split consductor 'new' into 'owner' and 'refer', split 'deref' into 'deref' and 'dered_mut'

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -3,12 +3,17 @@ mod text;
 mod note;
 
 trait Wrapped<T> {
-  fn new(raw: T) -> *mut Self;
+  fn owner(raw: T) -> *mut Self;
+  fn refer(raw: *mut T) -> *mut Self;
 
   fn raw(&self) -> &T;
   fn raw_mut(&mut self) -> &mut T;
 
-  unsafe fn deref(ptr: *mut Self) -> &'static mut Self {
+  unsafe fn deref(ptr: *mut Self) -> &'static Self {
+    &*ptr
+  }
+
+  unsafe fn deref_mut(ptr: *mut Self) -> &'static mut Self {
     &mut *ptr
   }
 }
