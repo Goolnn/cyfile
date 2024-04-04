150c7f9928b31adab6f3252949ade67a989bff64
Goolnn <Goolnn@hotmail.com>
2024-04-04 21:56:42 +0800

    refactor(ffi)!: change the method of ffi implemention

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,24 +1,10 @@
+use std::ffi::c_void;
+
 mod file;
 mod text;
 mod note;
 
-trait Wrapped<T> {
-  fn owner(raw: T) -> *mut Self;
-  fn refer(raw: *mut T) -> *mut Self;
-
-  fn raw(&self) -> &T;
-  fn raw_mut(&mut self) -> &mut T;
-
-  unsafe fn deref(ptr: *mut Self) -> &'static Self {
-    &*ptr
-  }
-
-  unsafe fn deref_mut(ptr: *mut Self) -> &'static mut Self {
-    &mut *ptr
-  }
-}
-
-enum Raw<T> {
-  Owner(T),
-  Refer(*mut T),
+#[no_mangle]
+unsafe extern fn cyfile_drop(ptr: *mut c_void) {
+  std::ptr::drop_in_place(ptr);
 }
--- src/ffi/text.rs
+++ src/ffi/text.rs
@@ -1,7 +1,4 @@
-use crate::ffi::{
-  Wrapped,
-  Raw,
-};
+use crate::Text;
 
 use std::ffi::{
   CString,
@@ -9,129 +6,57 @@ use std::ffi::{
   c_char,
 };
 
-pub struct Text {
-  raw: Raw<crate::Text>,
-
-  content: CString,
-  comment: CString,
-}
-
-impl Text {
-  pub unsafe fn set_content(&mut self, content: *const c_char) {
-    let content = CStr::from_ptr(content);
-
-    self.raw_mut().set_content(content.to_str().unwrap());
-
-    self.content = CString::from(content);
-  }
-
-  pub unsafe fn set_comment(&mut self, comment: *const c_char) {
-    let comment = CStr::from_ptr(comment);
-
-    self.raw_mut().set_comment(comment.to_str().unwrap());
-
-    self.comment = CString::from(comment);
-  }
-
-  pub fn content(&self) -> *const c_char {
-    self.content.as_ptr()
-  }
-
-  pub fn comment(&self) -> *const c_char {
-    self.comment.as_ptr()
-  }
-}
-
-impl Wrapped<crate::Text> for Text {
-  fn owner(raw: crate::Text) -> *mut Self {
-    let content = CString::new(raw.content()).unwrap();
-    let comment = CString::new(raw.comment()).unwrap();
-
-    Box::into_raw(Box::new(Self {
-      raw: Raw::Owner(raw),
-
-      content,
-      comment,
-    }))
-  }
-
-  fn refer(raw: *mut crate::Text) -> *mut Self {
-    let content = CString::new(unsafe { &*raw }.content()).unwrap();
-    let comment = CString::new(unsafe { &*raw }.comment()).unwrap();
-
-    Box::into_raw(Box::new(Self {
-      raw: Raw::Refer(raw),
-
-      content,
-      comment,
-    }))
-  }
-
-  fn raw(&self) -> &crate::Text {
-    unsafe {
-      match &self.raw {
-        Raw::Owner(owner) => owner,
-        Raw::Refer(refer) => &**refer,
-      }
-    }
-  }
-
-  fn raw_mut(&mut self) -> &mut crate::Text {
-    unsafe {
-      match &mut self.raw {
-        Raw::Owner(owner) => owner,
-        Raw::Refer(refer) => &mut **refer,
-      }
-    }
-  }
-}
-
 #[no_mangle]
 pub unsafe extern fn cyfile_text_new() -> *mut Text {
-  Text::owner(crate::Text::new())
+  Box::into_raw(Box::new(Text::new()))
 }
 
 #[no_mangle]
 pub unsafe extern fn cyfile_text_with_content_and_comment(content: *const c_char, comment: *const c_char) -> *mut Text {
-  Text::owner(crate::Text::with_content_and_comment(CStr::from_ptr(content).to_str().unwrap(), CStr::from_ptr(comment).to_str().unwrap()))
+  Box::into_raw(Box::new(Text::with_content_and_comment(CStr::from_ptr(content).to_string_lossy().as_ref(), CStr::from_ptr(comment).to_string_lossy().as_ref())))
 }
 
 #[no_mangle]
 pub unsafe extern fn cyfile_text_with_content(content: *const c_char) -> *mut Text {
-  Text::owner(crate::Text::with_content(CStr::from_ptr(content).to_str().unwrap()))
+  Box::into_raw(Box::new(Text::with_content(CStr::from_ptr(content).to_string_lossy().as_ref())))
 }
 
 #[no_mangle]
 pub unsafe extern fn cyfile_text_with_comment(comment: *const c_char) -> *mut Text {
-  Text::owner(crate::Text::with_comment(CStr::from_ptr(comment).to_str().unwrap()))
+  Box::into_raw(Box::new(Text::with_comment(CStr::from_ptr(comment).to_string_lossy().as_ref())))
 }
 
 #[no_mangle]
 pub unsafe extern fn cyfile_text_set_content(text: *mut Text, content: *const c_char) {
-  Text::deref_mut(text).set_content(content);
+  (*text).set_content(CStr::from_ptr(content).to_string_lossy().as_ref());
 }
 
 #[no_mangle]
 pub unsafe extern fn cyfile_text_set_comment(text: *mut Text, comment: *const c_char) {
-  Text::deref_mut(text).set_comment(comment);
+  (*text).set_comment(CStr::from_ptr(comment).to_string_lossy().as_ref());
 }
 
 #[no_mangle]
-pub unsafe extern fn cyfile_text_content(text: *mut Text) -> *const c_char {
-  Text::deref(text).content()
+pub unsafe extern fn cyfile_text_clear_content(text: *mut Text) {
+  (*text).clear_content();
 }
 
 #[no_mangle]
-pub unsafe extern fn cyfile_text_comment(text: *mut Text) -> *const c_char {
-  Text::deref(text).comment()
+pub unsafe extern fn cyfile_text_clear_comment(text: *mut Text) {
+  (*text).clear_comment();
+}
+
+#[no_mangle]
+pub unsafe extern fn cyfile_text_content(text: *mut Text) -> *mut c_char {
+  CString::new((*text).content()).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern fn cyfile_text_comment(text: *mut Text) -> *mut c_char {
+  CString::new((*text).comment()).unwrap().into_raw()
 }
 
 #[no_mangle]
 pub unsafe extern fn cyfile_text_debug(text: *mut Text) {
-  print!("{:?}", Text::deref(text).raw());
-}
-
-#[no_mangle]
-pub unsafe extern fn cyfile_text_drop(text: *mut Text) {
-  std::ptr::drop_in_place(text);
+  println!("{:?}", *text);
 }
