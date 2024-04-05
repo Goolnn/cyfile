79bbbfeeda4ee31162704197722be3361b9b24f5
Goolnn <Goolnn@hotmail.com>
2024-04-05 19:32:28 +0800

    feat(ffi): add function to drop ffi array

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,7 +1,10 @@
 use std::ffi::{
   CString,
+  c_void,
   c_char,
 };
+
+use std::ptr::drop_in_place;
 
 mod file;
 mod text;
@@ -10,5 +13,15 @@ mod page;
 
 #[no_mangle]
 unsafe extern fn cyfile_string_drop(string: *mut c_char) {
-  drop(CString::from_raw(string))
+  drop(CString::from_raw(string));
+}
+
+#[no_mangle]
+unsafe extern fn cyfile_array_mut_drop(array: *mut *mut c_void) {
+  drop_in_place(array);
+}
+
+#[no_mangle]
+unsafe extern fn cyfile_array_drop(array: *mut *const c_void) {
+  drop_in_place(array);
 }
