7d6c9e453caba053b10431e6d21227f81eebd655
Goolnn <Goolnn@hotmail.com>
2024-10-07 11:41:05 +0800

    feat(error): implement Error and Display traits for FileError

--- src/error/mod.rs
+++ src/error/mod.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+use std::fmt::Display;
 use std::io;
 
 pub type FileResult<T> = Result<T, FileError>;
@@ -21,5 +23,26 @@ pub enum FileError {
 impl From<io::Error> for FileError {
     fn from(_: io::Error) -> Self {
         Self::Undefined
+    }
+}
+
+impl Error for FileError {}
+
+impl Display for FileError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::InvalidHeader => write!(f, "invalid header"),
+            Self::InvalidVersion => write!(f, "invalid version"),
+            Self::InvalidStructure => write!(f, "invalid structure"),
+
+            Self::PathIsDirectory => write!(f, "path is directory"),
+            Self::PathNotExists => write!(f, "path not exists"),
+            Self::PathNotFile => write!(f, "path not file"),
+
+            Self::WriteFailed => write!(f, "write failed"),
+            Self::ReadFailed => write!(f, "read failed"),
+
+            Self::Undefined => write!(f, "undefined"),
+        }
     }
 }
