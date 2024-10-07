bb064185791d6f80b7adebd373cc2efb71c89d23
Goolnn <Goolnn@hotmail.com>
2024-10-07 19:04:51 +0800

    feat(export): add Exporter module for exporting project data

---
+++ src/export/arguments.rs
@@ -0,0 +1,23 @@
+use super::constants::VERSION_LATEST;
+use std::path::Path;
+use std::path::PathBuf;
+
+pub struct ExportArguments {
+    pub(super) filepath: PathBuf,
+    pub(super) version: (u8, u8),
+}
+
+impl ExportArguments {
+    pub fn new(filepath: impl AsRef<Path>) -> Self {
+        Self {
+            filepath: filepath.as_ref().to_path_buf(),
+            version: VERSION_LATEST,
+        }
+    }
+
+    pub fn with_version(mut self, version: (u8, u8)) -> Self {
+        self.version = version;
+
+        self
+    }
+}
---
+++ src/export/mod.rs
@@ -0,0 +1,53 @@
+pub mod constants;
+
+mod arguments;
+
+pub use arguments::ExportArguments;
+
+use crate::codec::Encode;
+use crate::codec::Writer;
+use crate::error::FileError;
+use crate::error::FileResult;
+use crate::Project;
+use constants::HEADER_DATA;
+use constants::VERSIONS;
+use std::fs::File;
+use std::io::Write;
+
+pub struct Exporter {
+    project: Project,
+    arguments: ExportArguments,
+}
+
+impl Exporter {
+    pub fn new(project: Project, arguments: ExportArguments) -> Self {
+        Self { project, arguments }
+    }
+
+    pub fn export(&self) -> FileResult<()> {
+        if self.arguments.filepath.is_file() {
+            return Err(FileError::PathNotFile);
+        }
+
+        let mut file = File::create(&self.arguments.filepath)?;
+
+        // 写入头部数据
+        file.write_all(&HEADER_DATA)?;
+
+        // 写入版本数据
+        if !VERSIONS.contains(&self.arguments.version) {
+            return Err(FileError::InvalidVersion);
+        }
+
+        let version = [self.arguments.version.0, self.arguments.version.1];
+
+        file.write_all(&version)?;
+
+        // 写入项目数据
+        let mut writer = Writer::with_version(file, self.arguments.version);
+
+        self.project.encode(&mut writer)?;
+
+        Ok(())
+    }
+}
