06dae9bab22032996712b9da9e5cd5e942ca0f56
Goolnn <Goolnn@hotmail.com>
2024-10-24 23:25:02 +0800

    feat(examples): improve example details

--- examples/create.rs
+++ examples/create.rs
@@ -1,3 +1,4 @@
+use cyfile::Credit;
 use cyfile::File;
 use cyfile::Note;
 use cyfile::Page;
@@ -6,6 +7,8 @@ use cyfile::Text;
 use std::fs;
 
 fn main() {
+    let cover = fs::read("tests/images/0.png").unwrap();
+
     let note_0 = Note::new()
         .with_coordinate(0.5, 0.5)
         .with_text(Text::new().with_content("content").with_comment("comment"));
@@ -16,7 +19,21 @@ fn main() {
 
     let image = fs::read("tests/images/0.png").unwrap();
     let page = Page::new(image).with_note(note_0).with_note(note_1);
-    let project = Project::new().with_title("Create").with_page(page);
+
+    let project = Project::new()
+        .with_cover(cover)
+        .with_category("Main Story")
+        .with_title("Create")
+        .with_credit(Credit::Artists, "Ayaka")
+        .with_credit(Credit::Translators, "Chongbenbao")
+        .with_credit(Credit::Proofreaders, "Jiangguo")
+        .with_credit(Credit::Retouchers, "Baishi")
+        .with_credit(Credit::Retouchers, "Zheerhui")
+        .with_credit(Credit::Typesetters, "Zheerhui")
+        .with_credit(Credit::Typesetters, "Goolnn")
+        .with_credit(Credit::Supervisors, "Chaibai")
+        .with_page(page);
+
     let file = File::create(project);
 
     println!("{:#?}", file.project());
