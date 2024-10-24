70efb9a9407e1a8678171389fa1b544724d7f380
Goolnn <Goolnn@hotmail.com>
2024-10-24 22:32:52 +0800

    feat(project): rewrite Project struct

--- src/project/mod.rs
+++ src/project/mod.rs
@@ -3,21 +3,31 @@ use crate::codec::Encode;
 use crate::codec::Reader;
 use crate::codec::Writer;
 use crate::error::FileError;
+use crate::Credit;
 use crate::Date;
 use crate::Note;
 use crate::Page;
 use crate::Text;
 use once_cell::sync::Lazy;
 use regex::Regex;
+use std::collections::HashMap;
+use std::collections::HashSet;
 use std::io::Read;
 use std::io::Write;
 
 #[derive(Default, PartialEq, Debug, Clone)]
 pub struct Project {
+    cover: Vec<u8>,
+
+    category: String,
     title: String,
+
+    number: (u32, u32),
 
     created_date: Date,
     saved_date: Date,
+
+    credits: HashMap<Credit, HashSet<String>>,
 
     pages: Vec<Page>,
 }
@@ -25,17 +35,51 @@ pub struct Project {
 impl Project {
     pub fn new() -> Self {
         Self {
+            cover: Vec::new(),
+
+            category: String::new(),
             title: String::new(),
+
+            number: (0, 0),
 
             created_date: Date::now(),
             saved_date: Date::now(),
+
+            credits: HashMap::new(),
 
             pages: Vec::new(),
         }
     }
 
+    pub fn with_cover(mut self, cover: Vec<u8>) -> Self {
+        self.cover = cover;
+
+        self
+    }
+
+    pub fn with_category(mut self, category: impl ToString) -> Self {
+        self.category = category.to_string();
+
+        self
+    }
+
     pub fn with_title(mut self, title: impl ToString) -> Self {
         self.title = title.to_string();
+
+        self
+    }
+
+    pub fn with_number(mut self, number: (u32, u32)) -> Self {
+        self.number = number;
+
+        self
+    }
+
+    pub fn with_credit(mut self, credit: Credit, name: impl ToString) -> Self {
+        self.credits
+            .entry(credit)
+            .or_default()
+            .insert(name.to_string());
 
         self
     }
@@ -46,12 +90,20 @@ impl Project {
         self
     }
 
-    pub fn set_title(&mut self, title: impl ToString) {
-        self.title = title.to_string();
+    pub fn cover(&self) -> &[u8] {
+        &self.cover
+    }
+
+    pub fn category(&self) -> &str {
+        &self.category
     }
 
     pub fn title(&self) -> &str {
         &self.title
+    }
+
+    pub fn number(&self) -> (u32, u32) {
+        self.number
     }
 
     pub fn created_date(&self) -> Date {
@@ -62,92 +114,20 @@ impl Project {
         self.saved_date
     }
 
+    pub fn credits_mut(&mut self) -> &mut HashMap<Credit, HashSet<String>> {
+        &mut self.credits
+    }
+
+    pub fn credits(&self) -> &HashMap<Credit, HashSet<String>> {
+        &self.credits
+    }
+
     pub fn pages_mut(&mut self) -> &mut Vec<Page> {
         &mut self.pages
     }
 
     pub fn pages(&self) -> &[Page] {
         &self.pages
-    }
-}
-
-impl Encode for Project {
-    fn encode<S: Write>(&self, writer: &mut Writer<S>) -> anyhow::Result<()> {
-        match writer.version().into() {
-            (0, 0) => {
-                writer.write_primitive(self.pages().len() as u8)?;
-
-                for page in self.pages() {
-                    writer.write_object(page)?;
-                }
-
-                Ok(())
-            }
-
-            (0, 1) => {
-                // 图像数量
-                writer.write_primitive(self.pages.len() as u8)?;
-
-                // 保存次数
-                writer.write_primitive(1_u8)?;
-
-                // 保存时间
-                Date::now().encode(writer)?;
-
-                // 图像数据
-                for page in self.pages() {
-                    writer.write_bytes_with_len::<u32>(page.data())?;
-                }
-
-                // 标记数据
-                for page in self.pages() {
-                    // 图像尺寸
-                    let (page_width, page_height) = page.size();
-
-                    // 标记数量
-                    writer.write_primitive(page.notes().len() as u8)?;
-
-                    for note in page.notes() {
-                        let note_x = (page_width as f64 * ((note.x() + 1.0) / 2.0)) as u16;
-                        let note_y = (page_height as f64 * (1.0 - (note.y() + 1.0) / 2.0)) as u16;
-
-                        writer.write_primitive(note_x)?;
-                        writer.write_primitive(note_y)?;
-
-                        // 合并文本
-                        let merged_text = note.merge_texts();
-
-                        // 初译数据
-                        writer.write_primitive(merged_text.len() as u16 + 1)?;
-                        writer.write_string_with_nil(&merged_text)?;
-                        // 校对数据
-                        writer.write_primitive(1u16)?;
-                        writer.write_string_with_nil("")?;
-                    }
-                }
-
-                Ok(())
-            }
-
-            (0, 2) => {
-                writer.write_string_with_len::<u32>(self.title())?;
-
-                writer.write_object(&self.created_date())?;
-                writer.write_object(&self.saved_date())?;
-
-                writer.write_primitive(self.pages().len() as u32)?;
-
-                for page in self.pages() {
-                    writer.write_object(page)?;
-                }
-
-                Ok(())
-            }
-
-            version => anyhow::bail!(FileError::UnsupportedVersion {
-                version: version.into()
-            }),
-        }
     }
 }
 
@@ -282,7 +262,13 @@ impl Decode for Project {
                 }
 
                 Ok(Self {
+                    cover: Vec::new(),
+
+                    category: String::new(),
                     title: String::new(),
+                    number: (0, 0),
+
+                    credits: HashMap::new(),
 
                     created_date: date,
                     saved_date: date,
@@ -291,32 +277,211 @@ impl Decode for Project {
                 })
             }
 
-            (0, 2) => {
-                let title = reader.read_string_with_len::<u32>()?;
+            (0, 2) => Ok(Self {
+                cover: reader.read_bytes_with_len::<u32>()?,
 
-                let created_date = reader.read_object()?;
-                let saved_date = reader.read_object()?;
+                category: reader.read_string_with_len::<u32>()?,
+                title: reader.read_string_with_len::<u32>()?,
 
-                let page_count = reader.read_primitive::<u32>()?;
+                number: reader.read_object()?,
 
-                let mut pages = Vec::with_capacity(page_count as usize);
+                created_date: reader.read_object()?,
+                saved_date: reader.read_object()?,
 
-                for _ in 0..page_count {
-                    pages.push(reader.read_object()?);
+                credits: reader.read_object()?,
+
+                pages: reader.read_object()?,
+            }),
+
+            version => anyhow::bail!(FileError::UnsupportedVersion {
+                version: version.into()
+            }),
+        }
+    }
+}
+
+impl Decode for (u32, u32) {
+    fn decode<S: Read>(reader: &mut Reader<S>) -> anyhow::Result<Self> {
+        let begin_number = reader.read_primitive()?;
+        let ent_number = reader.read_primitive()?;
+
+        Ok((begin_number, ent_number))
+    }
+}
+
+impl Decode for HashMap<Credit, HashSet<String>> {
+    fn decode<S: Read>(reader: &mut Reader<S>) -> anyhow::Result<Self> {
+        let mut credits = HashMap::new();
+
+        let credit_count = reader.read_primitive::<u8>()?;
+
+        for _ in 0..credit_count {
+            let credit = reader.read_object()?;
+            let names = reader.read_object()?;
+
+            credits.insert(credit, names);
+        }
+
+        Ok(credits)
+    }
+}
+
+impl Decode for HashSet<String> {
+    fn decode<S: Read>(reader: &mut Reader<S>) -> anyhow::Result<Self> {
+        let mut names = HashSet::new();
+
+        let name_count = reader.read_primitive::<u8>()?;
+
+        for _ in 0..name_count {
+            let name = reader.read_string_with_len::<u32>()?;
+
+            names.insert(name);
+        }
+
+        Ok(names)
+    }
+}
+
+impl Decode for Vec<Page> {
+    fn decode<S: Read>(reader: &mut Reader<S>) -> anyhow::Result<Self> {
+        let page_count = reader.read_primitive::<u32>()?;
+
+        let mut pages = Vec::with_capacity(page_count as usize);
+
+        for _ in 0..page_count {
+            pages.push(reader.read_object()?);
+        }
+
+        Ok(pages)
+    }
+}
+
+impl Encode for Project {
+    fn encode<S: Write>(&self, writer: &mut Writer<S>) -> anyhow::Result<()> {
+        match writer.version().into() {
+            (0, 0) => {
+                writer.write_primitive(self.pages().len() as u8)?;
+
+                for page in self.pages() {
+                    writer.write_object(page)?;
                 }
 
-                Ok(Self {
-                    title,
-                    created_date,
-                    saved_date,
-                    pages,
-                })
+                Ok(())
+            }
+
+            (0, 1) => {
+                // 图像数量
+                writer.write_primitive(self.pages.len() as u8)?;
+
+                // 保存次数
+                writer.write_primitive(1_u8)?;
+
+                // 保存时间
+                Date::now().encode(writer)?;
+
+                // 图像数据
+                for page in self.pages() {
+                    writer.write_bytes_with_len::<u32>(page.data())?;
+                }
+
+                // 标记数据
+                for page in self.pages() {
+                    // 图像尺寸
+                    let (page_width, page_height) = page.size();
+
+                    // 标记数量
+                    writer.write_primitive(page.notes().len() as u8)?;
+
+                    for note in page.notes() {
+                        let note_x = (page_width as f64 * ((note.x() + 1.0) / 2.0)) as u16;
+                        let note_y = (page_height as f64 * (1.0 - (note.y() + 1.0) / 2.0)) as u16;
+
+                        writer.write_primitive(note_x)?;
+                        writer.write_primitive(note_y)?;
+
+                        // 合并文本
+                        let merged_text = note.merge_texts();
+
+                        // 初译数据
+                        writer.write_primitive(merged_text.len() as u16 + 1)?;
+                        writer.write_string_with_nil(&merged_text)?;
+                        // 校对数据
+                        writer.write_primitive(1u16)?;
+                        writer.write_string_with_nil("")?;
+                    }
+                }
+
+                Ok(())
+            }
+
+            (0, 2) => {
+                writer.write_bytes_with_len::<u32>(&self.cover)?;
+
+                writer.write_string_with_len::<u32>(&self.category)?;
+                writer.write_string_with_len::<u32>(&self.title)?;
+
+                writer.write_object(&self.number)?;
+
+                writer.write_object(&self.created_date)?;
+                writer.write_object(&self.saved_date)?;
+
+                writer.write_object(&self.credits)?;
+
+                writer.write_object(&self.pages)?;
+
+                Ok(())
             }
 
             version => anyhow::bail!(FileError::UnsupportedVersion {
                 version: version.into()
             }),
         }
+    }
+}
+
+impl Encode for (u32, u32) {
+    fn encode<S: Write>(&self, writer: &mut Writer<S>) -> anyhow::Result<()> {
+        writer.write_primitive(self.0)?;
+        writer.write_primitive(self.1)?;
+
+        Ok(())
+    }
+}
+
+impl Encode for HashMap<Credit, HashSet<String>> {
+    fn encode<S: Write>(&self, writer: &mut Writer<S>) -> anyhow::Result<()> {
+        writer.write_primitive(self.len() as u8)?;
+
+        for (credit, names) in self.iter() {
+            writer.write_object(credit)?;
+            writer.write_object(names)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Encode for HashSet<String> {
+    fn encode<S: Write>(&self, writer: &mut Writer<S>) -> anyhow::Result<()> {
+        writer.write_primitive(self.len() as u8)?;
+
+        for name in self.iter() {
+            writer.write_string_with_len::<u32>(name)?;
+        }
+
+        Ok(())
+    }
+}
+
+impl Encode for Vec<Page> {
+    fn encode<S: Write>(&self, writer: &mut Writer<S>) -> anyhow::Result<()> {
+        writer.write_primitive(self.len() as u32)?;
+
+        for page in self.iter() {
+            writer.write_object(page)?;
+        }
+
+        Ok(())
     }
 }
 
