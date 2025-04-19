6d7e8a84de39906985f5c9ed0417068155d00ac8
Goolnn <Goolnn@hotmail.com>
2025-04-19 20:56:50 +0800

    feat(codec): implement error handling with custom Error enum

--- src/codec/error.rs
+++ src/codec/error.rs
@@ -1,6 +1,16 @@
+use std::io;
+use std::string;
 use thiserror::Error;
 
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Error)]
-pub enum Error {}
+pub enum Error {
+    #[error("failed to convert length while reading")]
+    InvalidLength,
+
+    #[error("an I/O error occurred: {0}")]
+    Io(#[from] io::Error),
+    #[error("an invalid UTF-8 sequence was found: {0}")]
+    Utf8(#[from] string::FromUtf8Error),
+}
