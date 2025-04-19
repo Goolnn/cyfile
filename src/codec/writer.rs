9e59e291dcbea28619c50cb4def45b3a85f0e4ee
Goolnn <Goolnn@hotmail.com>
2025-04-19 21:00:04 +0800

    refactor(writer): replace anyhow::Result with custom Result type

--- src/codec/writer.rs
+++ src/codec/writer.rs
@@ -1,9 +1,11 @@
 use crate::codec::bound::Length;
 use crate::codec::bound::Primitive;
+use crate::codec::error::Error;
+use crate::codec::error::Result;
 use crate::codec::Codec;
 use crate::file::VERSION_LATEST;
 use crate::Version;
-use std::io::Result;
+use std::io;
 use std::io::Seek;
 use std::io::SeekFrom;
 use std::io::Write;
@@ -38,14 +40,14 @@ where
         self.version
     }
 
-    pub fn write_object<T>(&mut self, object: &T) -> anyhow::Result<()>
+    pub fn write_object<T>(&mut self, object: &T) -> Result<()>
     where
         T: Codec,
     {
         object.encode(self)
     }
 
-    pub fn write_objects<L, T>(&mut self, objects: &Vec<T>) -> anyhow::Result<()>
+    pub fn write_objects<L, T>(&mut self, objects: &Vec<T>) -> Result<()>
     where
         L: Length,
         T: Codec,
@@ -59,7 +61,7 @@ where
         Ok(())
     }
 
-    pub fn write_primitive<T>(&mut self, data: T) -> anyhow::Result<()>
+    pub fn write_primitive<T>(&mut self, data: T) -> Result<()>
     where
         T: Primitive,
     {
@@ -73,13 +75,13 @@ where
         Ok(())
     }
 
-    pub fn write_bytes(&mut self, data: impl AsRef<[u8]>) -> anyhow::Result<()> {
+    pub fn write_bytes(&mut self, data: impl AsRef<[u8]>) -> Result<()> {
         self.stream.write_all(data.as_ref())?;
 
         Ok(())
     }
 
-    pub fn write_bytes_with_len<T>(&mut self, data: impl AsRef<[u8]>) -> anyhow::Result<()>
+    pub fn write_bytes_with_len<T>(&mut self, data: impl AsRef<[u8]>) -> Result<()>
     where
         T: Length,
     {
@@ -87,7 +89,7 @@ where
         self.write_bytes(data)
     }
 
-    pub fn write_string_with_len<T>(&mut self, data: &str) -> anyhow::Result<()>
+    pub fn write_string_with_len<T>(&mut self, data: &str) -> Result<()>
     where
         T: Length,
     {
@@ -95,19 +97,19 @@ where
         self.write_bytes(data.as_bytes())
     }
 
-    pub fn write_string_with_nil(&mut self, data: &str) -> anyhow::Result<()> {
+    pub fn write_string_with_nil(&mut self, data: &str) -> Result<()> {
         self.write_bytes(data.as_bytes())?;
         self.write_primitive::<u8>(0)
     }
 
-    fn write_len<T>(&mut self, len: usize) -> anyhow::Result<()>
+    fn write_len<T>(&mut self, len: usize) -> Result<()>
     where
         T: Length,
     {
         if let Ok(len) = len.try_into() {
             self.write_primitive::<T>(len)
         } else {
-            anyhow::bail!("failed to convert length while writing")
+            Err(Error::InvalidLength)
         }
     }
 
@@ -121,7 +123,7 @@ impl<S> Seek for Writer<S>
 where
     S: Write + Seek,
 {
-    fn seek(&mut self, pos: SeekFrom) -> Result<u64> {
+    fn seek(&mut self, pos: SeekFrom) -> io::Result<u64> {
         self.stream.seek(pos)
     }
 }
