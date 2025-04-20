8ad47032f3193dbddf6ab43d914b994b51e62604
Goolnn <Goolnn@hotmail.com>
2025-04-20 09:04:23 +0800

    feat(codec): add UnsupportedVersion and InvalidImage variants to Error enum

--- src/codec/error.rs
+++ src/codec/error.rs
@@ -1,3 +1,4 @@
+use crate::Version;
 use std::io;
 use std::string;
 use thiserror::Error;
@@ -8,9 +9,13 @@ pub type Result<T> = std::result::Result<T, Error>;
 pub enum Error {
     #[error("failed to convert length")]
     InvalidLength,
+    #[error("unsupported version: {version:?}")]
+    UnsupportedVersion { version: Version },
+    #[error("failed to parse image data")]
+    InvalidImage,
 
     #[error("an I/O error occurred: {0}")]
     Io(#[from] io::Error),
-    #[error("an invalid UTF-8 sequence was found: {0}")]
+    #[error("an utf-8 error occurred: {0}")]
     Utf8(#[from] string::FromUtf8Error),
 }
