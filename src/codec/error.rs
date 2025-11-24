292ea9d4311b9583e70b0e2cc636e92a9d90de51
Goolnn <Goolnn@hotmail.com>
2025-11-24 14:47:06 +0800

    feat: extend Error enum with UnsupportedImage and Undefined variants

--- src/codec/error.rs
+++ src/codec/error.rs
@@ -20,4 +20,19 @@ pub enum Error {
     Io(#[from] io::Error),
     #[error("an utf-8 error occurred: {0}")]
     Utf8(#[from] string::FromUtf8Error),
+
+    #[error("unsupported image format")]
+    UnsupportedImage,
+
+    #[error("undefined error")]
+    Undefined,
+}
+
+impl From<crate::image::error::Error> for Error {
+    fn from(value: crate::image::error::Error) -> Self {
+        match value {
+            crate::image::error::Error::Unsupported => Self::UnsupportedImage,
+            crate::image::error::Error::Undefined => Self::Undefined,
+        }
+    }
 }
