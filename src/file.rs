38cae5bf75ed78240d87f45450f8765461402b17
Goolnn <Goolnn@hotmail.com>
2026-02-04 15:41:48 +0800

    feat: reorganize module imports and expose error types

--- src/file.rs
+++ src/file.rs
@@ -1,6 +1,13 @@
 mod check;
+mod error;
 mod manifest;
+mod open;
+
+pub use error::Error;
+pub use error::Result;
 pub use manifest::Manifest;
 
 pub use check::check_from_path;
 pub use check::check_from_stream;
+pub use open::open_from_path;
+pub use open::open_from_stream;
