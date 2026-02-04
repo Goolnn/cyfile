f8f37221d914eeeb1d9aa1151cc8a7865c954231
Goolnn <Goolnn@hotmail.com>
2026-02-04 15:01:06 +0800

    feat: update Codec trait to use codec::Result and adjust decode method signature

--- src/codec.rs
+++ src/codec.rs
@@ -3,14 +3,13 @@ mod error;
 pub use error::Error;
 pub use error::Result;
 
+use crate::codec;
 use crate::file::Manifest;
 use serde_json::Value;
 use std::marker::Sized;
 
-pub trait Codec {
-    fn encode(&self, manifest: &Manifest) -> error::Result<Value>;
+pub trait Codec: Sized {
+    fn encode(&self, manifest: &Manifest) -> codec::Result<Value>;
 
-    fn decode(manifest: &Manifest, value: Value) -> error::Result<Self>
-    where
-        Self: Sized;
+    fn decode(manifest: &Manifest, value: &Value) -> codec::Result<Self>;
 }
