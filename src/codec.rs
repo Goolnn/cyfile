3432827e181a404b4778cf3e0e82b4d1ae311409
Goolnn <Goolnn@hotmail.com>
2026-02-04 15:51:10 +0800

    feat: implement Codec trait for Vec<T>

--- src/codec.rs
+++ src/codec.rs
@@ -13,3 +13,31 @@ pub trait Codec: Sized {
 
     fn decode(manifest: &Manifest, value: &Value) -> codec::Result<Self>;
 }
+
+impl<T: Codec> Codec for Vec<T> {
+    fn encode(&self, manifest: &Manifest) -> codec::Result<Value> {
+        let mut vec = Vec::with_capacity(self.len());
+
+        for item in self {
+            vec.push(item.encode(manifest)?);
+        }
+
+        Ok(Value::Array(vec))
+    }
+
+    fn decode(manifest: &Manifest, value: &Value) -> codec::Result<Self> {
+        let array = match value.as_array() {
+            Some(val) => val,
+
+            None => return Err(Error::ParseFailure),
+        };
+
+        let mut vec = Vec::with_capacity(array.len());
+
+        for item in array {
+            vec.push(T::decode(manifest, item)?);
+        }
+
+        Ok(vec)
+    }
+}
