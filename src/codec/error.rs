cebcf42cc7f3e8eff165766f13d9789f44da9d15
Goolnn <Goolnn@hotmail.com>
2026-02-04 17:16:10 +0800

    feat: define codec Error enum

--- src/codec/error.rs
+++ src/codec/error.rs
@@ -3,4 +3,10 @@ use thiserror::Error;
 pub type Result<T> = std::result::Result<T, Error>;
 
 #[derive(Debug, Error)]
-pub enum Error {}
+pub enum Error {
+    #[error("required field `{field}` is missing")]
+    FieldMissing { field: String },
+
+    #[error("expect {expected}, but found `{found}`")]
+    TypeMismatch { expected: String, found: String },
+}
