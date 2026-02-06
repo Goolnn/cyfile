dae95d0082385cb085624d8959880950c27df919
Goolnn <Goolnn@hotmail.com>
2026-02-06 10:25:58 +0800

    feat: enhance AssetSource trait and ArchiveSource struct with additional functionality

--- src/codec/asset.rs
+++ src/codec/asset.rs
@@ -1,35 +1,64 @@
 use crate::codec;
+use std::any::Any;
 use std::cell::RefCell;
 use std::io::Read;
 use std::io::Seek;
+use std::io::Write;
 use std::rc::Rc;
 use zip::ZipArchive;
+use zip::ZipWriter;
 
-pub trait AssetSource {
+pub trait AssetSource: Any {
     fn load(&self, path: &str) -> codec::Result<Vec<u8>>;
+
+    fn as_any(&self) -> &dyn Any;
 }
 
-pub struct ArchiveSource<S>
+pub struct ArchiveSource<R>
 where
-    S: Read + Seek,
+    R: Read + Seek,
 {
-    archive: Rc<RefCell<ZipArchive<S>>>,
+    archive: Rc<RefCell<ZipArchive<R>>>,
 }
 
-impl<S> ArchiveSource<S>
+impl<R> ArchiveSource<R>
 where
-    S: Read + Seek,
+    R: Read + Seek,
 {
-    pub fn new(archive: ZipArchive<S>) -> Self {
+    pub fn new(archive: ZipArchive<R>) -> Self {
         ArchiveSource {
             archive: Rc::new(RefCell::new(archive)),
         }
     }
+
+    pub fn copy<P, W>(&self, path: P, writer: &mut ZipWriter<W>) -> codec::Result<()>
+    where
+        P: AsRef<str>,
+        W: Write + Seek,
+    {
+        let mut archive = self.archive.borrow_mut();
+
+        let path = path.as_ref();
+
+        let stream = match archive.by_name(path) {
+            Ok(val) => val,
+
+            Err(_) => {
+                return Err(codec::Error::AssetNotFound {
+                    path: path.to_string(),
+                });
+            }
+        };
+
+        writer
+            .raw_copy_file(stream)
+            .map_err(|_| codec::Error::Undefined)
+    }
 }
 
-impl<S> AssetSource for ArchiveSource<S>
+impl<R> AssetSource for ArchiveSource<R>
 where
-    S: Read + Seek,
+    R: Read + Seek + 'static,
 {
     fn load(&self, path: &str) -> codec::Result<Vec<u8>> {
         let mut archive = self.archive.borrow_mut();
@@ -51,5 +80,9 @@ where
         }
 
         Ok(data)
+    }
+
+    fn as_any(&self) -> &dyn Any {
+        self
     }
 }
