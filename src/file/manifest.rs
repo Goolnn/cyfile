61c96566745dc0b34fb470e7f0b9240e76851c40
Goolnn <Goolnn@hotmail.com>
2026-02-07 13:40:08 +0800

    refactor: encapsulate version field in Manifest struct and improve method visibility

--- src/file/manifest.rs
+++ src/file/manifest.rs
@@ -3,5 +3,25 @@ use serde::Serialize;
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct Manifest {
-    pub version: u8,
+    version: u8,
+}
+
+impl Manifest {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn version(&self) -> u8 {
+        self.version
+    }
+
+    pub fn set_version(&mut self, version: u8) {
+        self.version = version;
+    }
+
+    pub fn with_version(mut self, version: u8) -> Self {
+        self.version = version;
+
+        self
+    }
 }
